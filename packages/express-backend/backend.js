35d8ad55b8cc1758769c289b22b1cdeea659e250
kyle wong <kyle9535@gmail.com>
2025-01-21 18:54:35 -0800

    added POST

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -41,6 +41,17 @@ app.get("/users/:id", (req, res) => {
 			      }
 });
 
+const addUser = (user) => {
+	  users["users_list"].push(user);
+	  return user;
+};
+
+app.post("/users", (req, res) => {
+	  const userToAdd = req.body;
+	  addUser(userToAdd);
+	  res.send();
+});
+
 const users = {
   users_list: [
     {
